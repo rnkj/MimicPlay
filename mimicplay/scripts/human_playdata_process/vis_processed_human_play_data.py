2f2562e2f4dc9abe654184101e229521bf96bf9d
Ryoichi Nakajo <ryoichi-nakajo@aist.go.jp>
2025-11-19 13:56:21 +0900

    batch processing of multiple videos for extracting human play data

--- mimicplay/scripts/human_playdata_process/vis_processed_human_play_data.py
+++ mimicplay/scripts/human_playdata_process/vis_processed_human_play_data.py
@@ -2,33 +2,49 @@ import h5py
 import numpy as np
 import cv2
 
-view_id = 1 # change to 2 if drawing on second view
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument("target_hdf5_path", type=str, help="HDF5 file to be visualized")
+parser.add_argument("--demo_idx", type=int, default=0, help="Index of demonstration")
+parser.add_argument("--view_idx", type=int, default=1, choices=[1, 2], help="Index of camera view")
+#parser.add_argument("--video_width", type=int, default=480, help="Width of video frames")
+#parser.add_argument("--video_height", type=int, default=640, help="Height of video frames")
+parser.add_argument("--video_fps", type=int, default=10, help="Frame per sec. of video")
+args = parser.parse_args()
 
 
-with h5py.File('demo_hand_loc_1_new.hdf5', 'r') as f:
-    images = np.array(f['data/demo_0/obs/front_image_{}'.format(view_id)])
-    actions = np.array(f['data/demo_0/actions'])
+with h5py.File(args.target_hdf5_path, "r") as f:
+    images = np.array(f[f"data/demo_{args.demo_idx}/obs/front_image_{args.view_idx}"])
+    actions = np.array(f[f"data/demo_{args.demo_idx}/actions"])
 
-# Reshape the actions to [145, 10, 4]
-actions = actions.reshape((145, 10, 4))
+height, width = images.shape[1:3]
+#print(f"[DEBUG] (height, width) = ({height}, {width})")
+#raise AssertionError
 
-if view_id == 1:
+# Reshape the actions to [T, 10, 4]
+T = len(actions)
+actions = actions.reshape((T, 10, 4))
+
+
+if args.view_idx == 1:
     actions = actions[:, :, :2]
-else:
+else:  # args.view_idx == 2
     actions = actions[:, :, 2:]
 
 
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-video = cv2.VideoWriter('output.mp4', fourcc, 30.0, (120, 120))
+fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+video = cv2.VideoWriter("output.mp4", fourcc, args.video_fps, (width, height))
 
 for i in range(images.shape[0]):
-    img = images[i].copy()
+    img = cv2.cvtColor(images[i].copy(), cv2.COLOR_RGB2BGR)
     action = actions[i]
 
-    action_unscaled = action * np.array([img.shape[1], img.shape[0]])
+    action_unscaled = action * np.array([width, height])
 
-    for pt in action_unscaled:
-        img = cv2.circle(img, (int(pt[1]), int(pt[0])), radius=5, color=(0, 255, 0), thickness=-1)
+    for i, pt in enumerate(action_unscaled):
+        g = int(255 - (255 - 100) * (i / (len(action_unscaled) - 1)))
+        img = cv2.circle(img, (int(pt[1]), int(pt[0])), radius=5, color=(0, g, 0), thickness=-1)
 
     video.write(img)
 
